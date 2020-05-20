a042f4729453ae14bbb185ad5308c5cde97973a6
Shrirup <banerjeeshrirup10@gmail.com>
2020-05-20 20:09:54 +0530

    Comments and Readme

---
+++ Readme.txt
@@ -0,0 +1,47 @@
+LIBRARIES USED : 
+
+React.JS
+React-beautiful-dnd
+uuid
+
+INSTRUCTIONS:
+
+1. Clone the repository
+2. Perform Yarn add or npm install in the cli inside the local directory of the repo.
+3. Then type yarn start to run gthe application on localhost:3000
+
+
+
+NOTE: If it doesnt render, replace package.json content with the following :
+
+{
+  "name": "react",
+  "version": "1.0.0",
+  "description": "",
+  "keywords": [],
+  "main": "src/index.js",
+  "dependencies": {
+    "react": "16.12.0",
+    "react-beautiful-dnd": "12.2.0",
+    "react-dom": "16.12.0",
+    "react-scripts": "3.0.1",
+    "uuid": "3.3.3"
+  },
+  "devDependencies": {
+    "typescript": "3.3.3"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test --env=jsdom",
+    "eject": "react-scripts eject"
+  },
+  "browserslist": [
+    ">0.2%",
+    "not dead",
+    "not ie <= 11",
+    "not op_mini all"
+  ]
+}
+
+Delete node modules folder from local dir and run YARN ADD again.
--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import uuid from "uuid/v4";
 
+//Use react-beautiful-dnd library to use the drag and drop components
+//uuid is used to assign a string based id to the columns on the board
+//The following is an array of all tasks present in the queue. Its a dummy database
+
 const itemsFromBackend = [
   { id: uuid(), content: "Task 1" },
   { id: uuid(), content: "Task 2" },
@@ -9,6 +13,8 @@ const itemsFromBackend = [
   { id: uuid(), content: "Task 4" },
   { id: uuid(), content: "Task 5" }
 ];
+
+//Below is the dummy columns to access on the frontend and the four stages of it are present
 
 const columnsFromBackend = {
   [uuid()]: {
@@ -28,6 +34,11 @@ const columnsFromBackend = {
     items: []
   }
 };
+
+//The function onDragEnd() is used to set the behaviour of cards while dragging and dropping
+//If no destination column is presnt, the card falls back to the source column
+//If a destination column is present, the card is dropped there by first assign the contents and id to source and dest respectively
+//Then the card is deleted from source column and assigned in the dest column at the required space
 
 const onDragEnd = (result, columns, setColumns) => {
   if (!result.destination) return;
@@ -65,6 +76,17 @@ const onDragEnd = (result, columns, setColumns) => {
     });
   }
 };
+
+//The following is the actual react render component where we are gonna declare nested <div> to break up the workspace
+//Assign necessary inline CSS and call the DragDropContext tag from the library to work with the Drag and Drop components
+//The first column in the div is the task queue which holds all the pending tasks
+//The size of all the columns are dynamic which makes the page look prettier
+//Create a nested div and in it, define the Droppable component with all details and inline css including snapshots.
+//Create a child component Draggable inside Droppable and again assign the required details and make it look snappy and responsive using snapshot properties
+//The Props in the page are accessed using the provided keyword
+//All these need to be in a Placeholder for it to render
+//When a card is hovered over a column it should highlight it and this is defined in the onDragEnd function
+
 
 function App() {
   const [columns, setColumns] = useState(columnsFromBackend);
